86cfe74bd407a8594bf8b478e13b52de13823710
'qianbiao' <'welanbiao@shu.edu.cn'>
2014-01-22 14:55:55 +0800

    最新改动
    初始界面

--- jiaoyouxitong/gen/org/view/R.java
+++ jiaoyouxitong/gen/org/view/R.java
@@ -5,7 +5,7 @@
  * should not be modified by hand.
  */
 
-package com.example.jiaoyouxitong;
+package org.view;
 
 public final class R {
     public static final class attr {
@@ -17,22 +17,27 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f030000;
-        public static final int activity_vertical_margin=0x7f030001;
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int first_view=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f070000;
+        public static final int action_settings=0x7f080001;
+        public static final int imageView1=0x7f080000;
+    }
+    public static final class layout {
+        public static final int activity_firstview=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f060000;
+        public static final int firstview=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f040001;
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040002;
+        public static final int action_settings=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050002;
     }
     public static final class style {
         /** 
@@ -45,10 +50,10 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
